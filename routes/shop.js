6a3dd2482a23807e0ab776ffdf83a8c447c9578a
joelpatel <joelpatel.cs@gmail.com>
2022-10-05 16:55:33 -0400

    / + get to match exact

--- routes/shop.js
+++ routes/shop.js
@@ -5,16 +5,15 @@ const router = express.Router();
 // use: allows us to add a new middleware function
 // inside the use will bascially be a function with 3 things
 // request, response & next
-router.use("/", (req, res, next) => {
+router.get("/", (req, res, next) => {
   //   console.log("route: /");
-  //   console.log("in the first middleware");
-  next(); // this allows the request to travel onto the next middleware (top -> bottom)
+  //   next(); // this allows the request to travel onto the next middleware (top -> bottom)
+  res.send("<h1>hello from express</h1>");
 });
 
-router.use((req, res, next) => {
-  //   console.log("in the second middleware");
-  res.send("<h1>hello from express</h1>");
-  //   console.log("response sent\n");
-});
+// router.use((req, res, next) => {
+//   //   console.log("in the second middleware");
+//   //   console.log("response sent\n");
+// });
 
 module.exports = router;
