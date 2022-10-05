d468ada55b96aa5140d7e0bbddace58664b7fcd8
joelpatel <joelpatel.cs@gmail.com>
2022-10-05 17:29:06 -0400

    filter routes -> /admin/add-product

--- app.js
+++ app.js
@@ -15,10 +15,12 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false })); // .urlencoded() yields a middleware function like below ones with next() call at the end but it also helps in getting the body of the request like we did in previous course sections
 
-app.use(adminRoutes);
+// app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
+  console.log("requested page not found");
   res.status(404).send("<h1>404 Page Not Found</h1>"); // send needs to be last
 });
 
--- routes/admin.js
+++ routes/admin.js
@@ -3,14 +3,17 @@ const express = require("express");
 const router = express.Router(); // mini express app, pluggable into other express apps (main app in app.js)
 
 // NOTE: IMP NOTE: .get() .post() etc. use exact match unlike .use() which matches the starting of the path ✅
+// /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   //   console.log("route: /add-product");
   res.send(
-    '<form action="/add-product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
+    // action can also be = "./add-product"
+    '<form action="./add-product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
   );
   //   res.send("<h2>2nd response</h2>"); // won't work, causes an error
 });
 
+// /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   console.log(req.body.title);
