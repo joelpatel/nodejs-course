86b3a34a4f989bfecdf0593af43c0db76753b9a5
joelpatel <joelpatel.cs@gmail.com>
2022-10-05 01:56:55 -0400

    simple http server

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req);
+  //   process.exit(0); to exit the event loop
 }); // this anonymous function will execute everytime a request reaches the server
 
 server.listen(5000);
