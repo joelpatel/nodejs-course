4a5b15445e921bc663c33fa181506a6f4f176ff7
joelpatel <joelpatel.cs@gmail.com>
2022-10-05 02:08:34 -0400

    simple http server

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html><head><title>Response Page</title></head>");
+  res.write("<body><h1>Hi</h1></body>");
+  res.write("</html>");
+  res.end();
+
   //   process.exit(0); to exit the event loop
 }); // this anonymous function will execute everytime a request reaches the server
 
