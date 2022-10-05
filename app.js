7a76f4f12048c734b6ac6ff71030ab713e795a59
joelpatel <joelpatel.cs@gmail.com>
2022-10-05 16:18:32 -0400

    form + routing + methods

--- app.js
+++ app.js
@@ -6,28 +6,39 @@
     this is the pluggable nature of express.js
 */
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/add-product", (req, res, next) => {
-  console.log("route: /add-product");
-  res.send("<h2>Product Added Successfully</h2>");
+app.use(bodyParser.urlencoded({ extended: false })); // .urlencoded() yields a middleware function like below ones with next() call at the end but it also helps in getting the body of the request like we did in previous course sections
+
+app.get("/add-product", (req, res, next) => {
+  //   console.log("route: /add-product");
+  res.send(
+    '<form action="/add-product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
+  );
   //   res.send("<h2>2nd response</h2>"); // won't work, causes an error
+});
+
+app.post("/add-product", (req, res, next) => {
+  console.log(req.body);
+  console.log(req.body.title);
+  res.redirect("/");
 });
 
 // use: allows us to add a new middleware function
 // inside the use will bascially be a function with 3 things
 // request, response & next
 app.use("/", (req, res, next) => {
-  console.log("route: /");
-  console.log("in the first middleware");
+  //   console.log("route: /");
+  //   console.log("in the first middleware");
   next(); // this allows the request to travel onto the next middleware (top -> bottom)
 });
 
 app.use((req, res, next) => {
-  console.log("in the second middleware");
+  //   console.log("in the second middleware");
   res.send("<h1>hello from express</h1>");
-  console.log("response sent\n");
+  //   console.log("response sent\n");
 });
 
 app.listen(5000);
