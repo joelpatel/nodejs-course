4cea5c6775ceca2150f70aa2a660f5f48c970e32
joelpatel <joelpatel.cs@gmail.com>
2022-10-06 20:06:13 -0400

    25-rest-advanced

--- 25-rest-backend/controllers/feed.js
+++ 25-rest-backend/controllers/feed.js
@@ -1,10 +1,25 @@
 const getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
-      { title: "First Post", content: "This is the content of my first post." },
       {
+        _id: Math.random().toString(),
+        title: "First Post",
+        content: "This is the content of my first post.",
+        imageURL: "images/image-0.png",
+        creator: {
+          name: "Max",
+        },
+        createdAt: new Date(),
+      },
+      {
+        _id: Math.random().toString(),
         title: "Second Post",
         content: "This is the content of my second post.",
+        imageURL: "images/image-0.png",
+        creator: {
+          name: "Max",
+        },
+        createdAt: new Date(),
       },
     ],
   });
@@ -15,7 +30,13 @@ const createPost = (req, res, next) => {
   const content = req.body.content;
   res.status(201).json({
     message: "Post created Successfully!",
-    post: { id: new Date().toISOString(), title, content },
+    post: {
+      _id: new Date().toISOString(),
+      title,
+      content,
+      creator: { name: "Max" },
+      createdAt: new Date(),
+    },
   });
 };
 
