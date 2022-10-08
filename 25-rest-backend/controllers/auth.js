8893cffb8a4d4c68ff2fbb90336a7bae014727ba
joelpatel <joelpatel.cs@gmail.com>
2022-10-08 19:44:37 -0400

    user signup

--- 25-rest-backend/controllers/auth.js
+++ 25-rest-backend/controllers/auth.js
@@ -1,4 +1,5 @@
 import { validationResult } from "express-validator";
+import bcrypt from "bcryptjs";
 
 import User from "../models/user.js";
 
@@ -6,7 +7,7 @@ const signup = (req, res, next) => {
   /**
    * checking if validation errors are present
    */
-  const errors = validationResult();
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error(
       "Validation failed. Invalid input. Please try again."
@@ -19,6 +20,29 @@ const signup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.email;
   const name = req.body.name;
+
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        email,
+        password: hashedPassword,
+        name,
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.status(201).json({
+        message: "User created!",
+        userID: result._id,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 export { signup };
