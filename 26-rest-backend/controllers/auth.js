b618f636081ec103fa2be10617a0cc969d006b0b
joelpatel <joelpatel.cs@gmail.com>
2022-10-09 19:35:52 -0400

    transforming to async await

--- 26-rest-backend/controllers/auth.js
+++ 26-rest-backend/controllers/auth.js
@@ -7,7 +7,7 @@ import User from "../models/user.js";
 
 config();
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   /**
    * checking if validation errors are present
    */
@@ -25,77 +25,70 @@ const signup = (req, res, next) => {
   const password = req.body.password;
   const name = req.body.name;
 
-  bcrypt
-    .hash(password, 12)
-    .then((hashedPassword) => {
-      const user = new User({
-        email,
-        password: hashedPassword,
-        name,
-      });
-      return user.save();
-    })
-    .then((result) => {
-      res.status(201).json({
-        message: "User created!",
-        userID: result._id,
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+  try {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
     });
+
+    const result = await user.save();
+    res.status(201).json({
+      message: "User created!",
+      userID: result._id,
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  let loadedUser;
-  User.findOne({ email: email })
-    .then((user) => {
-      if (!user) {
-        const err = new Error("Please enter valid information.");
-        err.statusCode = 401; // 401 -> not authenticated
-        throw err;
-      }
 
-      /**
-       * Valid email address.
-       * Now, validate the password.
-       */
-      loadedUser = user;
-      return bcrypt.compare(password, loadedUser.password);
-    })
-    .then((isEqual) => {
-      if (!isEqual) {
-        const err = new Error("Please enter valid information.");
-        err.statusCode = 401;
-        throw err;
-      }
-      /**
-       * email and password matches
-       */
-      const token = jwt.sign(
-        {
-          email: loadedUser.email,
-          userID: loadedUser._id.toString(),
-        },
-        process.env.JWT_PRIVATE_KEY,
-        { expiresIn: "1h" }
-      );
-      res.status(200).json({
-        token,
-        userID: loadedUser._id.toString(),
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      const err = new Error("Please enter valid information.");
+      err.statusCode = 401; // 401 -> not authenticated
+      throw err;
+    }
+
+    /**
+     * Valid email address.
+     * Now, validate the password.
+     */
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      const err = new Error("Please enter valid information.");
+      err.statusCode = 401;
+      throw err;
+    }
+    /**
+     * email and password matches
+     */
+    const token = jwt.sign(
+      {
+        email: user.email,
+        userID: user._id.toString(),
+      },
+      process.env.JWT_PRIVATE_KEY,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({
+      token,
+      userID: user._id.toString(),
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
 export { signup, login };
