1c76dc0bda9a5596980a9e0d1c10a3c1e13aa44a
joelpatel <joelpatel.cs@gmail.com>
2022-10-09 19:39:42 -0400

    transforming to async await

--- 26-rest-backend/controllers/status.js
+++ 26-rest-backend/controllers/status.js
@@ -2,7 +2,7 @@ import { validationResult } from "express-validator";
 
 import User from "../models/user.js";
 
-const updateStatus = (req, res, next) => {
+const updateStatus = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const err = new Error("Status not provided.");
@@ -11,50 +11,46 @@ const updateStatus = (req, res, next) => {
     throw err;
   }
 
-  User.findById(req.userIDFromJWTToken)
-    .then((user) => {
-      if (!user) {
-        const err = new Error("User not found.");
-        err.statusCode = 404;
-        throw err;
-      }
+  try {
+    const user = await User.findById(req.userIDFromJWTToken);
+    if (!user) {
+      const err = new Error("User not found.");
+      err.statusCode = 404;
+      throw err;
+    }
 
-      user.status = req.body.status;
-      return user.save();
-    })
-    .then((updatedUser) => {
-      res.status(200).json({
-        message: "status updated successfully",
-        status: updatedUser.status,
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+    user.status = req.body.status;
+    const updatedUser = await user.save();
+    res.status(200).json({
+      message: "status updated successfully",
+      status: updatedUser.status,
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
-const getStatus = (req, res, next) => {
-  User.findById(req.userIDFromJWTToken)
-    .then((user) => {
-      if (!user) {
-        const err = new Error("User not found.");
-        err.statusCode = 404;
-        throw err;
-      }
+const getStatus = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userIDFromJWTToken);
+    if (!user) {
+      const err = new Error("User not found.");
+      err.statusCode = 404;
+      throw err;
+    }
 
-      res.status(200).json({
-        status: user.status,
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+    res.status(200).json({
+      status: user.status,
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
 export { updateStatus, getStatus };
